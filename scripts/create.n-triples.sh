dc9b3d45218ac367651366b9bb75420568f8b785
Sarven Capadisli <info@csarven.ca>
2012-03-15 11:04:12 +0100

    Minor: Using new filename

--- scripts/create.n-triples.sh
+++ scripts/create.n-triples.sh
@@ -21,7 +21,7 @@ rapper -g /var/www/lib/worldbank-linkeddata/data/currencies.rdf >> /var/www/lib/
 
 rapper -g /var/www/lib/worldbank-linkeddata/data/worldbank-sameAs-dbpedia_countries.nt >> /var/www/lib/worldbank-linkeddata/data/import/graph.meta.nt ;
 
-rapper -g /var/www/lib/worldbank-linkeddata/data/worldbank-exactMatch-dbpedia_currencies.nt >> /var/www/lib/worldbank-linkeddata/data/import/graph.meta.nt ;
+rapper -g /var/www/lib/worldbank-linkeddata/data/worldbank-sameAs-dbpedia_currencies.nt >> /var/www/lib/worldbank-linkeddata/data/import/graph.meta.nt ;
 
 
 
