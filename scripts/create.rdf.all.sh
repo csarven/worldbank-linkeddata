367485ca5e3645ee5dcb5350e17b26d62b8abbd3
Sarven Capadisli <info@csarven.ca>
2012-03-25 17:44:50 +0100

    Added script to create all scripts. Sort of like one ring to all rings but not really

---
+++ scripts/create.rdf.all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+/var/www/lib/worldbank-linkeddata/scripts/./create.rdf.indicators.en.sh
+/var/www/lib/worldbank-linkeddata/scripts/./create.rdf.meta.sh
+/var/www/lib/worldbank-linkeddata/scripts/./create.rdf.finances-meta.sh
+/var/www/lib/worldbank-linkeddata/scripts/./create.rdf.finances.views.en.sh
+/var/www/lib/worldbank-linkeddata/scripts/./create.rdf.projects.en.sh
+
+#/var/www/lib/worldbank-linkeddata/scripts/./create.rdf.climates.en.sh
+#/var/www/lib/worldbank-linkeddata/scripts/./create.rdf.indicators.indicator.en.sh
