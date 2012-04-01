961c45d6ff39a6848482c8ba71abe1a64714a2d2
Sarven Capadisli <info@csarven.ca>
2012-04-01 23:21:10 +0100

    Added ensembles and variables for derives ensembles

--- scripts/create.rdf.climates.en.sh
+++ scripts/create.rdf.climates.en.sh
@@ -6,8 +6,8 @@ basePath="/var/www/lib/worldbank-linkeddata/data/climates/" ;
 pathToCountries="/var/www/lib/worldbank-linkeddata/data/indicators/en/countries.xml" ;
 
 areas=(country basin) ;
-statstypes=(mavg annualavg manom annualanom month-average-historical year-average-historical decade-average-historical) ;
-variables=(pr tas) ;
+statstypes=(mavg annualavg manom annualanom month-average-historical year-average-historical decade-average-historical mavg-ensemble annualavg-ensemble manom-ensemble annualanom-ensemble mavg-ensemble-derived annualavg-ensemble-derived manom-ensemble-derived annualanom-ensemble-derived) ;
+variables=(pr tas tmin_means tmax_means tmax_days90th tmin_days90th tmax_days10th tmin_days10th tmin_days0 ppt_days ppt_days2 ppt_days10 ppt_days90th ppt_dryspell ppt_means) ;
 
 for area in "${areas[@]}" ;
     do
@@ -15,7 +15,7 @@ for area in "${areas[@]}" ;
             do
                 for variable in "${variables[@]}" ;
                     do
-                        for file in "$basePath""$area"/"$statstype"/*."$variable".xml
+                        ls -1 "$basePath""$area"/"$statstype"/*."$variable".xml | while read file ;
                             do
                                 filename=$(basename $file);
                                 extension=${filename##*.};
