fd0e09c1d56a5b8d6fbbaa969a7de806b32481b2
Sarven Capadisli <info@csarven.ca>
2013-11-03 18:54:30 +0100

    Minor

--- scripts/tdbloader.world-bank.sh
+++ scripts/tdbloader.world-bank.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-java tdb.tdbloader --desc=/usr/lib/fuseki/tdb2.worldbank.ttl --graph=http://worldbank.270a.info/graph/meta ../data/import/graph.meta.nt
+state=".staging";
+tdbAssembler="/usr/lib/fuseki/tdb.worldbank$state.ttl";
+JVM_ARGS="-Xmx160000M";
 
-for i in ../data/meta/meta.finance*.rdf ;
-    do
-        java tdb.tdbloader --desc=/usr/lib/fuseki/tdb2.worldbank.ttl --graph=http://worldbank.270a.info/graph/meta "$i" ;
-    done
+#java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph=http://worldbank.270a.info/graph/world-bank-climates ../data/import/graph.world-bank-climates.nt
 
-java tdb.tdbloader --desc=/usr/lib/fuseki/tdb2.worldbank.ttl --graph=http://worldbank.270a.info/graph/world-bank-finances ../data/import/graph.world-bank-finances.nt
+java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph=http://worldbank.270a.info/graph/world-development-indicators /data/worldbank-linkeddata/data/import/graph.world-development-indicators.nt
 
-#java tdb.tdbloader --desc=/usr/lib/fuseki/tdb2.worldbank.ttl --graph=http://worldbank.270a.info/graph/world-development-indicators ../data/import/graph.world-development-indicators.nt
+#java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph=http://worldbank.270a.info/graph/world-bank-projects-and-operations ../data/import/graph.world-bank-projects-and-operations.nt
 
-java tdb.tdbloader --desc=/usr/lib/fuseki/tdb2.worldbank.ttl --graph=http://worldbank.270a.info/graph/world-bank-projects-and-operations ../data/import/graph.world-bank-projects-and-operations.nt
+#for i in ../data/meta/meta.finance*.rdf ; do java tdb.tdbloader --desc="$tdbAssembler" --graph=http://worldbank.270a.info/graph/meta "$i" ; done
 
+#java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph=http://worldbank.270a.info/graph/world-bank-finances ../data/import/graph.world-bank-finances.nt
+
+#java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph=http://worldbank.270a.info/graph/meta ../data/import/graph.meta.nt
+
+#java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph=http://worldbank.270a.info/graph/void /var/www/worldbank.270a.info/void.ttl
