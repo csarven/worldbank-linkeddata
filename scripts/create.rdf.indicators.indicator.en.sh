baa48fb8dceea356229bdb1a0c0565a5e0ed16f5
Sarven Capadisli <info@csarven.ca>
2016-10-13 23:33:25 +0000

    Init script to get metadata

--- scripts/create.rdf.indicators.indicator.en.sh
+++ scripts/create.rdf.indicators.indicator.en.sh
@@ -12,7 +12,8 @@ for file in ../data/indicators/en/indicator/*.xml;
         extension=${filename##*.};
         graph=${filename%.*};
 
-        java "$JVM_ARGS" net.sf.saxon.Transform -s "$file" -xsl ../scripts/xsl/indicatorsObservations.xsl wbapi_lang=en > ../data/indicators/en/indicator/"$graph".rdf
+#        java "$JVM_ARGS" net.sf.saxon.Transform -s "$file" -xsl ../scripts/xsl/indicatorsObservations.xsl wbapi_lang=en > ../data/indicators/en/indicator/"$graph".rdf
+        saxonb-xslt -t -tree:linked -ext:on -s "$file" -xsl ../scripts/xsl/indicatorsObservations.xsl wbapi_lang=en > ../data/indicators/en/indicator/"$graph".rdf
 
         echo "Created ../data/indicators/en/indicator/$graph.rdf"
     done;
